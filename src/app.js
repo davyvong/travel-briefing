f9e1e076ed10d1e58ffde5f495cf4dd8fafce37d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2022-05-31 17:59:14 -0400

    feat: add generate groupings method to fetch countries and find neighbors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,58 @@
-import { useCallback, useState } from 'react';
+import travelBriefingAPI from 'api/travel-briefing';
+import { useCallback, useEffect, useState } from 'react';
+import selectRandom from 'utils/select-random';
 
 const App = () => {
-  const [countries] = useState([]);
-  const [neighbors] = useState([]);
-  const [pending] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [neighbors, setNeighbors] = useState([]);
+  const [pending, setPending] = useState(false);
 
+  // Fetch data and calculate neighbors
+  const generateGroupings = useCallback(async () => {
+    setPending(true);
+    try {
+      // All requests to trave briefing are cached so we can repeatedly call the same endpoint.
+      const allCountries = await travelBriefingAPI.allCountries();
+      // Randomly select 10 countries
+      let selectedCountries = selectRandom(allCountries, 10);
+      setCountries(selectedCountries);
+      // Create a set of countries by name
+      const selectedCountryNames = new Set(selectedCountries.map(country => country.name));
+      // Fetch the API url of each selected country and cache the response
+      selectedCountries = await Promise.all(selectedCountries.map(country => travelBriefingAPI.get(country.url)))
+      const groupings = new Set();
+      // Loop through each selected country and their neighbours to see if the neighbours are selected countries
+      // 0 < selectedCountries is < 10
+      // 0 < country.neighbours < allCountries - 1 (but realistically there will not be even close to that many neighbors)
+      // Complexity is O(n * m), n = selectedCountries m = country.neighbours
+      selectedCountries.forEach(country => {
+        country.neighbors.forEach(neighbor => {
+          if (selectedCountryNames.has(neighbor.name)) {
+            // This is hacky, but we format the country pair alphabetically into a string and add it to a set so dupes don't occur
+            if (neighbor.name < country.names.name) {
+              groupings.add(`${neighbor.name} - ${country.names.name}`);
+            } else {
+              groupings.add(`${country.names.name} - ${neighbor.name}`);
+            }
+          }
+        });
+      });
+      setNeighbors(Array.from(groupings));
+    } catch (error) {
+      console.log(error);
+      setCountries([]);
+      setNeighbors([]);
+    } finally {
+      setPending(false);
+    }
+  }, []);
+
+  // Fetch data on component's first render
+  useEffect(() => {
+    generateGroupings();
+  }, [generateGroupings]);
+
+  // Render randomly selected 10 countries
   const renderCountries = useCallback(() => {
     if (pending) {
       return <p>Loading...</p>;
@@ -15,6 +63,7 @@ const App = () => {
     return <ul>{countries.map(country => <li key={country.name}>{country.name}</li>)}</ul>;
   }, [countries, pending]);
 
+  // Render neighbors
   const renderNeighbors = useCallback(() => {
     if (pending) {
       return <p>Loading...</p>;
@@ -27,7 +76,7 @@ const App = () => {
 
   return (
     <>
-      <button>Generate Groupings</button>
+      <button onClick={generateGroupings}>Generate Groupings</button>
       <h1>Selected Countries</h1>
       {renderCountries()}
       <h1>Neighbors</h1>
